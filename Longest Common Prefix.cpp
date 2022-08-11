2f373f56a4ffe092d8d59c729e107c769d1e8f65
Shrey <shrma.shray@gmail.com>
2022-08-11 19:44:04 +0530

    another solution

---
+++ Longest Common Prefix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string pref = "";
+        int smallest_len = INT_MAX;
+        for(int i=0;i<strs.size();i++){
+            if(strs[i].length()<smallest_len)
+                smallest_len=strs[i].length();
+        }
+        
+        
+        for(int i=0;i<smallest_len;i++){
+            char s = strs[0][i];
+            for(int j=0;j<strs.size();j++){
+                    if(s!=strs[j][i]){
+                        return pref;
+                    }
+               
+            }
+             pref+=s;
+        }
+        return pref;
+    }
+};
